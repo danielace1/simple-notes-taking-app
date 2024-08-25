d4ce22fe7795729168344efc288b03a36c697590
danielace1 <danielsudharsan1805@gmail.com>
2024-08-25 23:54:36 +0530

    init

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
