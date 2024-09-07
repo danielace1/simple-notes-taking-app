4e33fbdac6701b25f5ff8bb671577ca6916e3efe
danielace1 <danielsudharsan1805@gmail.com>
2024-09-08 00:45:07 +0530

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <div>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+    </div>
+  );
+}
+
+export default App;
