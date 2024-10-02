7990687f6894754edf8159d5c29743944d3fdac6
danielace1 <danielsudharsan1805@gmail.com>
2024-10-02 17:54:19 +0530

    feat: server completed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import NoteRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import env from "./util/validateEnv";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
 
@@ -10,7 +14,23 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGODB_CONNECTION_STRING,
+    }),
+  })
+);
+
 app.use("/api/notes", NoteRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found!"));
